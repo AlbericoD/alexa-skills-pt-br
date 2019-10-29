8a4bc1872144870dcdad5e4f46e717740a5dfab1
AlbericoD <albericodiasbarretofilho@gmail.com>
2019-10-29 16:51:18 -0300

    adicionado arquivos de falas. adicionado sdk da alexa. adcionado manipuladores de eventos.

---
+++ falas.js
@@ -0,0 +1,8 @@
+const falas = {
+    BEM_VINDO: 'OLá, seja bem vindo, o que você deseja hoje?',
+    FATOS: 'A água não tem cheiro e nem gosto',
+    ERRO: 'Deu ruim, concerta ai mano'
+}
+
+
+module.exports = falas;
--- handler.js
+++ handler.js
@@ -1,18 +1,53 @@
-'use strict';
+const Alexa = require('ask-sdk-core');
+const { BEM_VINDO, FATOS, ERRO } = require('./falas');
 
-module.exports.hello = async event => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+const LaunchRequestHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
+  },
+  handle(handlerInput) {
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+
+    return handlerInput.responseBuilder
+      .speak(BEM_VINDO)
+      .reprompt(BEM_VINDO)
+      .withSimpleCard('Titulo', BEM_VINDO)
+      .getResponse();
+  }
+}
+
+const FatosSobreAguaIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && handlerInput.requestEnvelope.request.intent.name === 'FatosSobreAgua';
+  },
+  handle(handlerInput) {
+
+
+    return handlerInput.responseBuilder
+      .speak(FATOS)
+      .withSimpleCard('FATOS', FATOS)
+      .getResponse();
+  }
 };
+
+const ErrorHandler = {
+  canHandle() {
+    return true;
+  },
+  handle(handlerInput, error) {
+    console.log(`Error handled: ${error.message}`);
+
+    return handlerInput.responseBuilder
+      .speak(ERRO)
+      .reprompt(ERRO)
+      .getResponse();
+  },
+};
+
+module.exports.hello = Alexa.SkillBuilders.custom()
+  .addRequestHandlers(
+    LaunchRequestHandler,
+    FatosSobreAguaIntentHandler)
+  .addErrorHandlers(ErrorHandler)
+  .lambda();
